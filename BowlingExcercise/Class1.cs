3d77dc47f7f7a9760d9b515e9a11d4245ac9add2
rogerjd <rjpd@netzero.com>
2019-03-04 20:57:54 -0500

    Start()

--- BowlingExcercise/Class1.cs
+++ BowlingExcercise/Class1.cs
@@ -6,7 +6,61 @@ using System.Threading.Tasks;
 
 namespace BowlingExcercise
 {
-    public class Class1
+    interface IGame
     {
+        void Start();
+        void Roll(int numOfPins);
+        int[] ScoreByFrame();
+        int TotalScore { get; }
+
+    }
+    public class BowlingScorer : IGame
+    {
+        int[] Frames = new int[10];
+
+        int IGame.TotalScore => throw new NotImplementedException();
+
+        public int Add()
+        {
+            return 0;
+        }
+
+        void IGame.Roll(int numOfPins)
+        {
+            throw new NotImplementedException();
+        }
+
+        int[] IGame.ScoreByFrame()
+        {
+            throw new NotImplementedException();
+        }
+
+        void IGame.Start()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class Game
+    {
+        int[] Frames = new int[10];
+        int currentFrame;
+
+        public void Start()
+        {
+            Array.Clear(Frames, 0, Frames.Length);
+            currentFrame = 1;
+        }
+
+        public void Roll(int NumberOfPins)
+        {
+
+        }
+
+        public int[] ScoreByFrame => Frames;
+
+        public int TotalScore { get; }
+
+        public int CurrentFrame => currentFrame;
     }
 }
---
+++ BowlingExcerciseTests/GameTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BowlingExcercise;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BowlingExcercise;
+
+namespace BowlingExcercise.Tests
+{
+    [TestClass()]
+    public class GameTests
+    {
+        [TestMethod()]
+        public void StartTest()
+        {
+            Game game = new Game();
+            game.Start();
+
+            Assert.AreEqual(game.CurrentFrame, 1);
+
+            Assert.AreEqual(game.ScoreByFrame.Length, 10);
+
+            CollectionAssert.AreEqual(game.ScoreByFrame, new int[10]);
+        }
+    }
+}
